512bbf260b64383db8d535abf4ba5c1b023737c4
Jojo Bautista <gjojob@yahoo.com>
2021-10-20 23:39:28 -0500

    shop input to use integer

--- assets/js/game.js
+++ assets/js/game.js
@@ -139,17 +139,16 @@ var shop = function() {
     var shopOptionPrompt = window.prompt(
         "Would you like to REFILL your health, UPGRADE your attack or LEAVE the store? Please eneter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice.");
     
+    // convert  shoptOptionPrompt value from string to integer
+    shopOptionPrompt = parseInt(shopOptionPrompt);
     switch(shopOptionPrompt){
-        case "REFILL":
-        case "refill":
+        case 1:
             playerInfo.refillHealth();
             break;
-        case "UPGRADE":
-        case "upgrade":
+        case 2:
             playerInfo.upgradeAttack();
             break;
-        case "LEAVE":
-        case "leave":
+        case 3:
             window.alert("Leaving the store.");
 
             // do nothing, so function will end
